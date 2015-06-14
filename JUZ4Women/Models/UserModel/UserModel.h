c59d9166afa573c4197cd7fed3c0de73ddc966c6
Kailash <kailashn@intelligrape.com>
2015-06-14 17:17:00 +0530

    User model filling method added.

--- JUZ4Women/Models/UserModel/UserModel.h
+++ JUZ4Women/Models/UserModel/UserModel.h
@@ -14,20 +14,31 @@
 @interface UserModel : NSObject
 
 @property(nonatomic, strong) NSString *userId;
-
 @property(nonatomic, strong) NSString *realName;
+
 @property(nonatomic, strong) NSString *displayName;
-
 @property(nonatomic, strong) NSString *emailAddress;
+
 @property(nonatomic, strong) NSNumber *mobileNumber;
-
 @property(nonatomic, strong) NSString *password;
+
 @property(nonatomic, strong) NSNumber *verificationCode;
-
 @property(nonatomic, strong) DateModel *dateOfBirth;
-@property(nonatomic, assign) SignUpOption signUpOption;
 
+@property(nonatomic, assign) SignUpOption signUpOption;
 @property(nonatomic, strong) NSString *city;
+
 @property(nonatomic, strong) NSString *country;
+@property (nonatomic ,strong) NSString *imageUrl;
+
+@property (nonatomic,strong) NSString *userName;
+
+@property (assign) long userTotalPostCount;
+@property (assign) long userTotalComments;
+@property (assign) long userTotalProfileViews;
+@property (assign) long userTotalPostShares;
+
+
+-(id)initWithUserId:(NSString *)userId withRealName:(NSString *)realName withDisplayName:(NSString *)displayName withEmailId:(NSString *)emailId withMobileNumber:(NSNumber *)mobileNumber withPassword:(NSString *)password withVerificationCode:(NSNumber *)code withDateModel:(DateModel *)dateModel withSignUpOption:(SignUpOption)option withCityName:(NSString *)city withCountry:(NSString *)country withImageUrl:(NSString *)imageUrl withUserName:(NSString *)userName withPostCount:(long)postCount withCommentsCount:(long)commentsCount withProfileViewsCount:(long)profileViewsCount withPostShareCount:(long)postShareCount;
 
 @end
